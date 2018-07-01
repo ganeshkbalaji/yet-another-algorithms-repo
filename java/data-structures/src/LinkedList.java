73ad432c4986c4c09a819b5244354039698bc7c5
Ganesh Balaji <ganeshkbalaji@ymail.com>
2018-07-01 15:16:22 -0700

    inital commit linked list

---
+++ java/data-structures/src/LinkedList.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ganeshkb on 7/1/18.
+ */
+public class LinkedList {
+
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d){ data = d; };
+    }
+}
