04c1a79d58f24e871d79dbbfef2f609905777aab
Ganesh Balaji <ganeshkbalaji@ymail.com>
2018-07-02 22:34:38 -0700

    added linked list example

---
+++ java/Data-structures/Linked-list/src/LinkedListImplementation.java
@@ -0,0 +1,15 @@
+/**
+ * Created by ganeshkb on 7/1/18.
+ */
+
+
+
+public class LinkedListImplementation {
+    // Linked List node
+    class Node {
+        int data;
+        Node next;
+
+        Node(int d) {data = d;}
+    }
+}
